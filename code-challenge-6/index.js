6647c02bf5b476a9709078e583ebfabda964ec1e
KenPlearnscoding <126117889+KenLearnsCoding@users.noreply.github.com>
2023-10-30 22:38:15 -0400

    push

--- code-challenge-6/index.js
+++ code-challenge-6/index.js
@@ -1,6 +1,9 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://catsdb:<password>@cluster0.r9tghgm.mongodb.net/?retryWrites=true&w=majority";
+const axios = require('axios');
+const fs = require('fs');
+const uri = "mongodb+srv://catsdb:n01404222@catsdb.sunyqht.mongodb.net/?retryWrites=true&w=majority";
+const dbName = 'catsdb';
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -15,6 +18,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    console.log('Connected to MongoDB');
+
+    // Select the 'catsdb' database
+    const db = client.db(dbName);
+
+    // Read the JSON files and parse push them to the mongodb collections
+    const jsonData_breeds = JSON.parse(fs.readFileSync('code-challenge-6/cat_breeds.json', 'utf8'));
+    const jsonData_facts = JSON.parse(fs.readFileSync('code-challenge-6/cat_facts.json', 'utf8'));
+    
+    // Create or access the 'facts' and 'breeds' collections
+    const factsCollection = db.collection('facts');
+    const breedsCollection = db.collection('breeds');
+
+    // Save 'data' objects from the JSON file into 'facts' collection
+    await factsCollection.insertMany(jsonData_facts.data);
+    await breedsCollection.insertMany(jsonData_breeds.breeds);
+    console.log('Data inserted into MongoDB collections.');
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
