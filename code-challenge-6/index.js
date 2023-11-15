07dac6adca78aa0ce35adf34402388ffcc82cba4
KenPlearnscoding <126117889+KenLearnsCoding@users.noreply.github.com>
2023-11-14 22:41:09 -0500

    push

--- code-challenge-6/index.js
+++ code-challenge-6/index.js
@@ -1,21 +1,20 @@
-
 const { MongoClient } = require('mongodb');
 const fs = require('fs');
-const uri = "mongodb+srv://catsdb:n01404222@catsdb.sunyqht.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://catsdb:n01404222@database.npb5sor.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
 async function run() {
     // Connect the client to the server	
-    await client.connect();
-    console.log('Connected to MongoDB');
+    // await client.connect();
+    // console.log('Connected to MongoDB');
    
     // Select the 'catsdb' database
-    const dbName = "catsdb";
+    const dbName = 'catsdb';
     const db = client.db(dbName);
     
     // Read the JSON files and parse before pushing them to the mongodb collections
-    const jsonData_breeds = JSON.parse(fs.readFileSync('code-challenge-6/cat_breeds.json', 'utf8'));
-    const jsonData_facts = JSON.parse(fs.readFileSync('code-challenge-6/cat_facts.json', 'utf8'));
+    const jsonData_breeds = JSON.parse(fs.readFileSync('./cat_breeds.json'));
+    const jsonData_facts = JSON.parse(fs.readFileSync('./cat_facts.json'));
     
     // Access the 'facts' and 'breeds' collections
     const breedsCollection = db.collection("breeds");
@@ -23,24 +22,24 @@ async function run() {
     
     //I will add 'data' objects to the proper collections first and then add the 'image' objects with random cat URL to the 'breeds' collection
 
+    async function getRandomCatGif() {
+        const response = await fetch( "https://thecatapi.com/api/images/get?format=src&type=gif");
+        return response.url;
+    }
+        
+    for (const entry in jsonData_breeds.data) {
+        
+        await getRandomCatGif().then((getLink) => {
+            jsonData_breeds.data[entry].image = getLink;
+        });
+        
+    }
+    
     // Save 'data' objects from the JSON file into 'facts' collection
     await factsCollection.insertMany(jsonData_facts.data);
     // Save 'data' objects from the JSON file into 'breeds' collection
     await breedsCollection.insertMany(jsonData_breeds.data);
     console.log('Data inserted into MongoDB collections.');
 
-
-    // Create random 'image' URLs for 'breeds' and save them. In the breeds collection, objects are represented
-    // in 1 level of the data structure, so I will use the map function to iterate through the array of objects.
-    // This is also really cool when i found this algorithm (line 36-39) from internet. 
-    const breedData = jsonData_breeds.breeds.map(breed => ({
-        name: 'image',
-        data: 'https://thecatapi.com/api/images/get?format=src&type=gif',
-    }));
-    //insert the 'image' objects into the 'breeds' collection
-    await breedsCollection.insertMany(breedData);
-  
-    //close the server
-     client.close();
 }
-run()
+run();
